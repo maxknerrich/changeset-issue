dc045c85492248eb99a81b8e455e484a39b64130
Benjamin Mehl <Benni.mehlBM@gmail.com>
2023-05-12 13:19:32 +0200

    feat: uno css attributify

--- apps/app/src/routes/+page.svelte
+++ apps/app/src/routes/+page.svelte
@@ -3,4 +3,19 @@
 	Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
 </p>
 
-<a href="/another">Go to another page</a>
+<div>
+	<button
+		bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
+		text="sm white"
+		font="mono bold"
+		p="y-2 x-4"
+		border="2 rounded blue-200"
+		rounded="lg"
+	>
+		Button
+	<a 
+		href="/another"
+		>Go to another page</a>
+	</button>
+</div>
+
--- apps/app/uno.config.ts
+++ apps/app/uno.config.ts
@@ -1,3 +1,5 @@
-import { defineConfig } from 'unocss';
+import { presetAttributify, presetUno, defineConfig } from 'unocss';
 
-export default defineConfig({});
+export default defineConfig({
+	presets: [presetAttributify({}), presetUno()],
+});
